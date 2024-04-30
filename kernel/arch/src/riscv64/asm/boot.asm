c2465702931a303fc5ba45e45a8ba1296a06c98f
Haoud <cadilhac.romain@gmail.com>
2024-04-30 22:17:05 +0200

    Removed `riscv-rt` dependency
    This allow more flexibility for the future

---
+++ kernel/arch/src/riscv64/asm/boot.asm
@@ -0,0 +1,11 @@
+.section .init
+.globl _start
+_start:
+  la sp, boot_stack_top
+  call entry
+
+# Reserve 8 KiB for the boot stack
+.section .bss
+boot_stack_bottom:
+.space 8192
+boot_stack_top:
--- kernel/arch/src/riscv64/lang.rs
+++ kernel/arch/src/riscv64/lang.rs
@@ -1,5 +1,12 @@
 use super::log;
 
+core::arch::global_asm!(include_str!("asm/boot.asm"));
+
+extern "C" {
+    static __bss_start: [u8; 0];
+    static __bss_end: [u8; 0];
+}
+
 /// Oops ! The kernel panicked and must be stopped. Since we are developing a
 /// microkernel, this should never happen. If it does, it means that there is a
 /// bug in the kernel. It will print some information about the panic if the
@@ -13,8 +20,15 @@ fn panic(_info: &core::panic::PanicInfo) -> ! {
 /// The entry point of the kernel. It will call architecture-specific setup
 /// and then call the `kiwi` function which is the main function of the kernel
 /// that will properly start the kernel and never return.
-#[riscv_rt::entry]
-unsafe fn _start(hart: usize, device_tree: usize) -> ! {
+#[no_mangle]
+unsafe extern "C" fn entry(hart: usize, device_tree: usize) -> ! {
+    // Clear the BSS section
+    let bss_start = core::ptr::addr_of!(__bss_start) as usize;
+    let bss_end = core::ptr::addr_of!(__bss_end) as usize;
+    let bss_size = bss_end - bss_start;
+    core::ptr::write_bytes(core::ptr::addr_of!(__bss_start) as *mut u8, 0, bss_size);
+
+    // Setup the architecture-specific stuff and start the kernel
     super::setup(hart, device_tree as *const u8);
     crate::kiwi();
 }
