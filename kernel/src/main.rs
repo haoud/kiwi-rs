cb166dd428001fb520b48adea61a0fb4bab03c1e
Haoud <cadilhac.romain@gmail.com>
2024-05-03 18:53:55 +0200

    [WIP] Task structure

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -1,9 +1,16 @@
 #![no_std]
 #![no_main]
 
+pub mod task;
+
 /// The `kiwi` function is called after the architecture-specific initialization
 /// was completed. It is responsible for setting up the kernel and starting the
 /// first user-space process.
+///
+/// # Safety
+/// This function should only be called once during the kernel boot process. Once
+/// the boot process is completed, the function will be wiped from memory to free
+/// up memory space.
 #[macros::init]
 #[no_mangle]
 pub fn kiwi() -> ! {
