1c8d164340cde0314f72d54df6a14f18b66b8888
Haoud <cadilhac.romain@gmail.com>
2026-02-22 17:36:40 +0100

    Better panic message

--- kernel/src/arch/x86_64/lang.rs
+++ kernel/src/arch/x86_64/lang.rs
@@ -1,4 +1,14 @@
+use crate::arch;
+
 #[panic_handler]
-fn panic(_info: &core::panic::PanicInfo) -> ! {
-    loop {}
+fn panic(info: &core::panic::PanicInfo) -> ! {
+    let cpu_id = arch::smp::cpu_identifier();
+    let message = info.message();
+    if let Some(location) = info.location() {
+        log::error!("[CPU {}] {} at {}", cpu_id, message, location);
+    } else {
+        log::error!("[CPU {}] {}", cpu_id, message);
+    }
+
+    arch::cpu::freeze();
 }
